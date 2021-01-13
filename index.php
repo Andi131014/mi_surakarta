0dff05435da4c128677c0d850de70c726a5cb59b
andisurakarta <andi.amikomsurakarta@gmail.com>
2021-01-13 07:39:26 +0700

    test commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MI_AMIKOM</title>
+</head>
+<body>
+    <h3> Management Informatika</h3>
+    <h3> STMIK AMIKOM SURAKARTA</h3>
+</body>
+</html>
